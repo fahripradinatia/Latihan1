3d5274a039ca037c9bf13cf4232b874d8a4df302
fahripradinatia <56189241+fahripradinatia@users.noreply.github.com>
2019-10-25 01:44:31 +0700

    Tugas

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+#Ini Variabel & Value
+
+namalengkap = "Fahri Pradinatia"
+namapanggilan = "Fahri"
+npm = "311910647"
+tempatlahir = "Bekasi"
+umur = "21"
+telf = "085774279526"
+alamat = "Bekasi Timur"
+
+print("Assalamualaikum, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,". My NPM is",npm,". I was born in",tempatlahir,"and i am",umur,"years old. I am very glad if you want to invite my house in",alamat,". So, don't forget to call me before with the number",telf,".\n Thank You")
